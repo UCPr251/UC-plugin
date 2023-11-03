393b79af796418ae9b5ae2ccb8e74556f72651d0
UCPr <2032385471@qq.com>
2023-11-03 14:20:56 +0800

    重命名一下

---
+++ apps/randomMember.js
@@ -0,0 +1,31 @@
+import { UCPr, Permission } from '../components/index.js'
+import { segment } from 'icqq'
+
+export class UCRandomMember extends plugin {
+  constructor() {
+    super({
+      name: 'UC-randomMember',
+      dsc: '随机挑选一个幸运群友',
+      event: 'message.group',
+      priority: UCPr.priority,
+      rule: [
+        {
+          reg: '^#?随机群友$',
+          fnc: 'randomMember'
+        }
+      ]
+    })
+  }
+
+  async randomMember(e) {
+    const cfg = UCPr.randomMember
+    if (!cfg.isOpen) return false
+    if (!Permission.verify(e, cfg)) return false
+    const map = await e.group.getMemberMap()
+    const memberArr = Array.from(map.values())
+    const mem = memberArr[Math.ceil(Math.random() * memberArr.length)]
+    const msg = [cfg.reply.replace('info', `${mem.nickname}（${mem.user_id}）`), segment.image(`https://q1.qlogo.cn/g?b=qq&s=0&nk=${mem.user_id}`)]
+    if (cfg.isAt) msg.unshift(segment.at(mem.user_id))
+    return e.reply(msg)
+  }
+}
