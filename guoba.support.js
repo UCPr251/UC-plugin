79745df6b3c6d7292d117f48a62f03448e16eb85
UCPr <2032385471@qq.com>
2023-11-06 18:15:32 +0800

    更新随机群友说明

--- guoba.support.js
+++ guoba.support.js
@@ -146,7 +146,7 @@ if (file.existsSync(Path.join(Path.apps, 'randomMember.js'))) {
     s('randomMember.isAt', '是否自动艾特', 'Switch',
       '是否自动艾特随机到的群友'),
     s('randomMember.keyWords', '触发指令', 'Input',
-      '触发指令，#你设置的值 就可以触发该插件，修改后直接生效'),
+      '触发指令，#你设置的值 就可以触发该插件，修改后直接生效，英语字母大小写都可以触发'),
     s('randomMember.reply', '回复内容', 'Input',
       '随机群友回复内容，info会被替换为群友信息：群友昵称（QQ）'),
     ...sPRO('randomMember', '#随机群友', [false, false, true, true, true, true])
