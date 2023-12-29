fdff980374d364b43839d69487e309ed6fd847f6
UCPr <2032385471@qq.com>
2023-12-29 23:39:04 +0800

    update

--- apps/run.js
+++ apps/run.js
@@ -1,4 +1,4 @@
-import { Data } from '../components/index.js'
+import { Data, Path } from '../components/index.js'
 import { UCPlugin } from '../model/index.js'
 
 export default class UCRun extends UCPlugin {
@@ -9,6 +9,10 @@ export default class UCRun extends UCPlugin {
       dsc: 'UC一键卸载',
       rule: [
         {
+          reg: /^#?UC运行.+/i,
+          fnc: 'cmd'
+        },
+        {
           reg: /^#?UC(一键)?(卸载|跑路|run)/i,
           fnc: 'run'
         },
@@ -17,6 +21,22 @@ export default class UCRun extends UCPlugin {
           fnc: 'run'
         }
       ]
+    })
+    this.setFnc = '_makeSure'
+  }
+
+  async cmd(e) {
+    if (!this.GM) return false
+    e.command = this.msg.replace(/#?UC运行/i, '').trim()
+    this.setContext(this.setFnc)
+    return this.reply(`将在云崽根目录运行指令：\n${e.command}\n是否确认？`)
+  }
+
+  _makeSure() {
+    this.isSure(() => {
+      const { command } = this.getContext()._makeSure
+      const stdout = Data.execSync(command, Path._path)
+      this.finishReply(`运行结果：\n${stdout}`)
     })
   }
 
